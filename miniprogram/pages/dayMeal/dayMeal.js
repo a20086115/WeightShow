b9d0ff4feee300e20aa2b55ed9c03a6a15295e12
Yu wenqiang <yuwenqiang@topscomm.com>
2020-06-11 19:49:08 +0800

    就餐优化 引入模板

--- miniprogram/pages/dayMeal/dayMeal.js
+++ miniprogram/pages/dayMeal/dayMeal.js
@@ -14,63 +14,28 @@ Page({
     },{
       name: "晚餐",
       totalCalorie:200
+    }],
+    currentFoods:[{
+      name: "早餐",
+      totalCalorie:200
+    },{
+      name: "午餐",
+      totalCalorie:200
+    },{
+      name: "晚餐",
+      totalCalorie:200
     }]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    console.log(options)
-    // 设置日期
+  onChange(e) {
+    this.setData({
+      value: e.detail,
+    });
+  },
+  onSearch() {
+    Toast('搜索' + this.data.value);
+  },
+  onClick() {
+    Toast('搜索' + this.data.value);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
