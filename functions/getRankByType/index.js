863d396848fd63fe0e3ecedd3a33760c1b7e7eb2
Yu wenqiang <yuwenqiang@topscomm.com>
2020-06-11 17:08:18 +0800

    排行榜优化

---
+++ functions/getRankByType/index.js
@@ -0,0 +1,25 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+// cloud.init({
+//   env: cloud.DYNAMIC_CURRENT_ENV
+// })
+cloud.init()
+const db = cloud.database()
+const _ = db.command
+// 根据表名和query对象查询数据
+exports.main = async (event, context) => {
+  let openId = event.userInfo.openId; // 调用人的openid
+  let type = event.type; // 要查询的类型 0-day, 1-周， 2-月
+  try {
+    return await db.collection('users')
+      .where({
+        
+      })
+      .orderBy('_id', 'desc')
+      .limit(12)
+      .get()
+
+  } catch (e) {
+    console.error(e)
+  }
+}
